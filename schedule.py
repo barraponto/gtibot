4b915b79a43b2a653bc56cc101e36406bcdf2687
Capi Etheriel <barraponto@gmail.com>
2018-02-21 15:13:48 -0300

    write the schedule into a simple list

---
+++ schedule.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+
+Class = namedtuple('Class', 'subject teacher room')
+
+SCHEDULE = [
+    # Monday is the 0 index
+    [ Class('Processos Gerenciais', 'Sandra Ossada', 'Lab 3'),
+      Class('Matemática Discreta', 'Aurimar Reis', 'Sala 21') ],
+    # Tuesday
+    [ Class('Inglês I', 'Marlucy Ribeiro', 'Sala 8'),
+      Class('Fundamentos de TI', 'Anderson Coan', 'Lab 3') ],
+    # Wednesday
+    [ Class('Algoritmos', 'Maria Janaína Ferreira', 'Lab 3'),
+      Class('Algoritmos', 'Maria Janaína Ferreira', 'Lab 3') ],
+    # Thursday
+    [ Class('Processos Gerenciais', 'Sandra Ossada', 'Lab 3'),
+      Class('Matemática Discreta', 'Aurimar Reis', 'Sala 21') ],
+    # Friday
+    [ Class('Comunicação e Expressão', 'Flávio Pereira', 'Sala 17'),
+      Class('Comunicação e Expressão', 'Flávio Pereira', 'Sala 17') ],
+    # Saturday
+    [ Class('Atividades ACC', 'Diogo Robles', 'Sala 18'),
+      Class('Atividades ACC', 'Diogo Robles', 'Sala 18') ]
+]
