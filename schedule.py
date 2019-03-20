06b27f89af473bf1c3601a78862e758d6af73106
CalisTI <46696171+Calistu@users.noreply.github.com>
2019-03-20 20:02:02 -0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -4,20 +4,20 @@ Class = namedtuple('Class', 'subject teacher room')
 
 SCHEDULE = [
     # Monday is the 0 index
-    [ Class('Gestão Ambiental', 'ELIANE MELO', 'Sala 18'),
-      Class('Gestão de Pessoas', 'SANDRA APARECIDA', 'lab 3') ],
+    [ Class('Gestão Ambiental', 'Eliane Melo', 'Sala 18'),
+      Class('Gestão de Pessoas', 'Sandra Aparecida', 'lab 3') ],
     # Tuesday
     [ Class('Inglês III', 'Thiago Rebeca', 'Sala 19'),
       Class('Contabilidade', 'Tessa Coltro', 'Sala 18') ],
     # Wednesday
-    [ Class('Gestão de Pessoas', 'SANDRA APARECIDA', 'Lab 3'),
+    [ Class('Gestão de Pessoas', 'Sandra Aparecida', 'Lab 3'),
       Class('Estatística', 'Diogo Robies', 'Sala 18') ],
     # Thursday
-    [ Class('Banco de Dados', 'MARIA JANAINA', 'Lab 1'),
-      Class('Banco de Dados', 'MARIA JANAINA', 'Lab 1') ],
+    [ Class('Banco de Dados', 'Maria Janaina', 'Lab 1'),
+      Class('Banco de Dados', 'Maria Janaina', 'Lab 1') ],
     # Friday
-    [ Class('Engenharia de Software', 'ANDERSON BARBOSA', 'Sala 2'),
-      Class('Engenharia de Software', 'ANDERSON BARBOSA', 'Sala 2') ],
+    [ Class('Engenharia de Software', 'Aanderson Barbosa', 'Sala 2'),
+      Class('Engenharia de Software', 'Anderson Barbosa', 'Sala 2') ],
     # Saturday
-    [ Class('Atividades de Projetos II/III', 'ANDERSON BARBOSA', 'Lab 3') ]
+    [ Class('Atividades de Projetos II/III', 'Anderson Barbosa', 'Lab 3') ]
 ]
